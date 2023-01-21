4733136f9247811a4a1ad69d729dc856eff040f5
dim419 <6696056@gmail.com>
2023-01-21 20:56:22 +0300

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const inpArr = [];
+    for (let i = 0; i < expr.length; i += 10) {
+    inpArr.push(expr.slice(i, i + 10));
+    }
+
+    const newArr = Object.keys(MORSE_TABLE);
+    const newArr2 = Object.values(MORSE_TABLE);
+    const encript = [];
+    for (i = 0; i < newArr.length; i++) {
+    encript.push(newArr[i].replace(/\./g, '10'));
+    }
+    const arr2 = encript.map(function(value) {
+    return value.replace(/-/g, '11').padStart(10, '0');
+    });
+
+
+    let arr3 = [];
+    for (let i = 0; i < inpArr.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+    if (inpArr[i] === '**********') {
+        arr3.push(' ');
+    }
+        if(inpArr[i] === arr2[j]) {
+        arr3.push(newArr2[j]);
+        } 
+    }
+    }
+    const finStr = arr3.join('').replace(/ +/g, ' ').trim();
+    return finStr;
 }
 
 module.exports = {
